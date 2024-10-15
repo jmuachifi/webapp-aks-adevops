efacddc2a014601b70037762c1a4d9ff452e03e2
Jodionisio Muachifi <44368068+jmuachifi@users.noreply.github.com>
2024-10-15 13:44:09 +0100

    Update main.tf

--- aks/terraform/main.tf
+++ aks/terraform/main.tf
@@ -35,7 +35,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     admin_username = "ubuntu"
 
     ssh_key {
-      key_data = jsondecode(azapi_resource_action.ssh_public_key_gen.output).publicKey
+      key_data = azapi_resource_action.ssh_public_key_gen.output.publicKey
     }
   }
   network_profile {
